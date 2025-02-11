f5504a872ff5a6d4a09983be3b909118f99e76b8
Aman Dixit <amandixitbsp@gmail.com>
2025-02-11 21:15:17 +0530

    version 2

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -20,13 +20,48 @@ export const createTask = async (req,res) =>{
 }
 
 export const getTask = async (req,res) =>{
- return res.json({message:"TASKS"});
+    try {
+        const tasks = await todoModel.find({
+            creator:req.user._id,
+        });
+
+        return res.status(200).json(tasks);   
+    } catch (error) {
+        console.log("error is in GETTASK controller:", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+    }
+
 }
 
 export const deleteTask = async (req,res) =>{
+    const { id: taskId} =   req.params;
+    
+    try {
+        await todoModel.findByIdAndDelete(taskId);
 
+        return res.status(200).json({message:"TASK DELETED SUCCESFULLY"})
+    } catch (error) {
+        console.log("error is in TASK delete controller:", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+    }
 }
 
 export const updateTask = async (req,res)  =>{
-
+    try {
+        const { id: taskId } = req.params;
+        const todo = await todoModel.findById(taskId);
+    
+        if (!todo) {
+          return res.status(400).json({ message: "task does not exist" });
+        }
+    
+        todo.isComplete = !todo.isComplete;
+    
+        await todo.save();
+    
+        return res.status(200).json({ message: "status upadated successfully" });
+      } catch (error) {
+        console.log("error is in isCompleted todo controller:", error.message);
+        res.status(500).json({ message: "Internal Server Error" });
+      }
 }
